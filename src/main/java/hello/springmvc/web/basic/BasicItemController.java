19a281e4797ecf16787fa7b681ae21144739d7db
joyeongbin <3170024@kbfg.com>
2022-02-21 12:58:15 +0900

    #Day3 Added form Request Handler and removed ModelAttribute

--- src/main/java/hello/springmvc/web/basic/BasicItemController.java
+++ src/main/java/hello/springmvc/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -27,11 +28,82 @@ public class BasicItemController {
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable Long itemId, Model model){
+    public String item(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findBy(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    /**
+     * ModelAttribute 쓰기전!!
+     *
+     * @param itemName
+     * @param price
+     * @param quantity
+     * @param model
+     * @return
+
+     @PostMapping("/add") public String addItemV1(@RequestParam String itemName,
+     @RequestParam int price,
+     @RequestParam Integer quantity,
+     Model model) {
+     Item item = new Item();
+     item.setItemName(itemName);
+     item.setPrice(price);
+     item.setQuantity(quantity);
+
+     itemRepository.save(item);
+     model.addAttribute("item", item);
+
+     return "basic/item";
+     }
+     */
+
+    /**
+     * @param item
+     * @return
+     * @ModelAttribute 는 Request의 RequestParam도 처리하고, Response로 Model에 View 영역도 같은 이름으로 넣어준다.
+
+     @PostMapping("/add") public String addItemV2(@ModelAttribute("item") Item item) {
+     // Model model) {
+     itemRepository.save(item);
+     //@ModelAttribute 는 자동으로 View 에 자동으로 넣어준다. "item"이라는 이름으로.
+     //그래서 param에 model날려도되고, addAttribute도 필요가없다.
+     //model.addAttribute("item", item);
+
+     return "basic/item";
+     }*/
+
+    /**
+     * @param item
+     * @return
+     * @ModelAttribute 의 ("name") 도 생략
+
+     @PostMapping("/add") public String addItemV3(@ModelAttribute Item item) {
+     itemRepository.save(item);
+     //@ModelAttribute 의 ()를 생략하면 Item -> item 으로 name이 자동생성된다.
+
+     return "basic/item";
+     }
+     */
+
+    /**
+     * @param item
+     * @return
+     * @ModelAttribute까지도 생략가능
+     */
+    @PostMapping("/add")
+    public String addItemV3(Item item) {
+        itemRepository.save(item);
+        // @ModelAttribute를 생략하면 Item을 RequestParam, Response 객체로 인식하고
+        // Item -> item 으로 name을 자동생성까지 해준다.
+
+        return "basic/item";
     }
 
     /**
